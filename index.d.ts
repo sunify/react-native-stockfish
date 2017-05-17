ffb3ae8173889f1994c335cd901b341482f882bf
Alex Lunyov <isuntc@gmail.com>
2017-05-17 23:48:06 +0800

    Fix declaration

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-import { EventEmitter2 } from 'eventemitter2';
-
 declare module 'react-native-stockfish' {
+  import { EventEmitter2 } from 'eventemitter2';
+
   class Engine extends EventEmitter2 {
     public sendCommand(command: string): void;
     public commit(): void;
